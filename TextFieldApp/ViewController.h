a587234c3b4fcd0aef5d4cde23428469b3c8f5b0
Ziyad Parekh <ziyad@1stdibs.com>
2014-10-24 09:44:23 -0400

    Initial Commit

---
+++ TextFieldApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TextFieldApp
+//
+//  Created by Ziyad Parekh on 10/24/14.
+//  Copyright (c) 2014 Ziyad Parekh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TextFieldApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TextFieldApp
+//
+//  Created by Ziyad Parekh on 10/24/14.
+//  Copyright (c) 2014 Ziyad Parekh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
